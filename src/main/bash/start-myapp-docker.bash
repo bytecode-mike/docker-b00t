20dfc1e03461d6057f402e1ada30310698e234c7
Svarog <bytecode.mike@gmail.com>
2015-07-05 19:29:50 +0200

    the mysql profile is localhost, in this way the application can be used as local application (without docker).

--- src/main/bash/start-myapp-docker.bash
+++ src/main/bash/start-myapp-docker.bash
@@ -2,7 +2,7 @@
 #set -xv
 
 docker run --name perk0nswebapp \
-           --link perkonsmysql:mysql \
+           --link perkonsmysql:localhost \
            -d \
            -p 8080:8080 \
            perk0ns-web
