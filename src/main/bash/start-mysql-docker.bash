f01c11fb9b34c62ab1e5cd725ef1ac02bfdef7e4
Svarog <bytecode.mike@gmail.com>
2015-08-18 02:53:30 +0200

    expose the mysql port

--- src/main/bash/start-mysql-docker.bash
+++ src/main/bash/start-mysql-docker.bash
@@ -7,6 +7,7 @@ docker run --name perkonsmysql \
            -e MYSQL_DATABASE=perkons_db \
            -e MYSQL_ROOT_PASSWORD=root \
            -d \
+           -p 3306:3306 \
            mysql:latest
 
 
